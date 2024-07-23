627261d2f7bfe13282815021ace295353468a012
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-07-24 01:25:58 +0900

    [Bronze III] Title: 직각삼각형, Time: 64 ms, Memory: 5524 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.cs
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cs
@@ -1,29 +1,12 @@
-using System;
-using System.Security.Principal;
-
-
-class Program
+string s;
+while((s = Console.ReadLine()) != "0 0 0")
 {
-    static void Main(string[] args)
+    string[] lines = s.Split(' ');
+    int[] line = new int[3];
+    for (int i = 0; i < 3; i++)
     {
-        string s;
-        while((s = Console.ReadLine()) != "0 0 0")
-        {
-            string[] lines = s.Split(' ');
-            int[] line = new int[3];
-            for (int i = 0; i < 3; i++)
-            {
-                line[i] = int.Parse(lines[i]);
-            }
-            Array.Sort(line);
-            if ((line[2] * line[2]) == (line[1]*line[1]) + (line[0] * line[0]))
-            {
-                Console.WriteLine("right");
-            }
-            else
-            {
-                Console.WriteLine("wrong"); 
-            }
-        }
+        line[i] = int.Parse(lines[i]);
     }
+    Array.Sort(line);
+    Console.WriteLine((line[2] * line[2]) == (line[1] * line[1]) + (line[0] * line[0]) ? "right" : "wrong");
 }
