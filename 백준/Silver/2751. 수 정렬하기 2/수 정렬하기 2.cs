2b1139dce4c78284abf8526690862cff22b972fd
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-07-29 12:39:33 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 476 ms, Memory: 23768 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cs
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cs
@@ -1,5 +1,5 @@
-StreamReader sr = new StreamReader(Console.OpenStandardInput());
-StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
 int N = int.Parse(sr.ReadLine());
 int[] arr = new int[N];
 for (int i = 0; i < N; i++)
