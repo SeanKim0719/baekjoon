2192e1b6f89c261feaca678cf4b21829ec5d12a2
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-07-30 15:16:57 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 108 ms, Memory: 5692 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cs
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cs
@@ -1,33 +1,18 @@
 int[] num = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 int N = num[0];
 int K = num[1];
+int index = 0;
 List<int> list = new List<int>();
 Queue<int> queue = new Queue<int>();
 for(int i = 1; i <= N; i++)
 {
     list.Add(i);
 }
-list.TrimExcess();
-for (int i = K-1; list.Count > 0; i += K-1)
-{ 
-    if(i >= list.Count)
-    {
-        while (i >= list.Count)
-        {
-            i -= list.Count;
-        }
-        queue.Enqueue(list[i]);
-        list.RemoveAt(i);
-    }
-    else
-    {
-        queue.Enqueue(list[i]);
-        list.RemoveAt(i);
-    }
-}
-for (int i = 0; i < list.Count; i++) 
+while(list.Count>0)
 {
-    queue.Enqueue(list[i]);
+    index = (index + K - 1) % list.Count;
+    queue.Enqueue(list[index]);
+    list.RemoveAt(index);
 }
 if (queue.Count == 1)
 {
