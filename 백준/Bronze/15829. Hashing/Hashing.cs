e780fc5b1bfddee58b30c68b66b541acb8e5fdae
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-08-02 20:37:59 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 5644 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cs
@@ -0,0 +1,12 @@
+using System.Numerics;
+
+var L = int.Parse(Console.ReadLine());
+var str = Console.ReadLine();
+BigInteger r = 1;
+BigInteger sum = 0;
+foreach(var item in str)
+    {
+        sum += (item - 'a' + 1) * r;
+        r *= 31;
+    }
+Console.WriteLine(sum);
