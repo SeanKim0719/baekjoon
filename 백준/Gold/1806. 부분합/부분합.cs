46d505eb3928e5abb2a46a0b4d9d6bc97ffa11a8
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-08-08 23:18:38 +0900

    [Gold IV] Title: 부분합, Time: 72 ms, Memory: 13216 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cs
+++ 백준/Gold/1806. 부분합/부분합.cs
@@ -1,32 +1,24 @@
-var input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int N = input[0]; int S = input[1];
-var arr = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int l = 0; int r = 0; int sum = 0;
 List<int> len = new List<int>();
-while(r <= arr.Length && l < arr.Length)
+while (l <= r)
 {
-    if (r == arr.Length)
+    if (sum < S)
+    {
+        if (r == arr.Length) break;
+        sum += arr[r];
+        r++;
+    }
+    else
     {
         sum -= arr[l];
         l++;
-    }
-    else 
-    {
-        if (sum < S)
-        {
-            sum += arr[r];
-            r++;
-        }
-        else
-        {
-            sum -= arr[l];
-            l++;
-        }
     }
     if (sum >= S)
     {
         len.Add(r - l);
     }
 }
-
 Console.WriteLine(len.Count != 0 ? len.Min() : "0");
