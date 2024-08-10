134e2a22344864b085d032cdfb231c26cae8d227
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-08-10 19:07:28 +0900

    [Silver IV] Title: 동전 0, Time: 48 ms, Memory: 5540 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cs
+++ 백준/Silver/11047. 동전 0/동전 0.cs
@@ -2,18 +2,13 @@ var input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
 int N = input[0]; int K = input[1];
 int[] coins = new int[N];
 int count = 0;
-int n = N - 1;
 for (int i = 0; i < N; i++)
 {
     coins[i] = int.Parse(Console.ReadLine());
 }
-while (K > 0)
+for(int i = N - 1; i >= 0; i--)
 {
-    while(K >= coins[n])
-    {
-        K -= coins[n];
-        count++;
-    }
-    n--;
+    count += K / coins[i];
+    K %= coins[i];
 }
 Console.WriteLine(count);
