e8909ec787b00f7fb9e98c5122271cd441d3c3ac
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-08-15 13:20:04 +0900

    [Silver V] Title: 집합, Time: 840 ms, Memory: 20028 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cs
+++ 백준/Silver/11723. 집합/집합.cs
@@ -2,34 +2,40 @@ StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
 StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
 int[] arr = new int[20];
 int N = int.Parse(sr.ReadLine());
-for(int i = 0; i < N; i++) {
+for (int i = 0; i < N; i++)
+{
     var com = sr.ReadLine().Split();
-    if (com[0] == "add") add(int.Parse(com[1]), arr);
-    else if (com[0] == "remove") remove(int.Parse(com[1]), arr);
-    else if (com[0] == "check") sw.WriteLine(check(int.Parse(com[1]), arr));
-    else if (com[0] == "toggle") toggle(int.Parse(com[1]), arr);
-    else if (com[0] == "all") all(arr);
-    else if (com[0] == "empty") empty(arr);
+    int x = com.Length > 1 ? int.Parse(com[1]) : 0;
+    switch (com[0])
+    {
+        case "add": add(x, arr); break;
+        case "remove": remove(x,arr); break;
+        case "check": sw.WriteLine(check(x,arr)); break;
+        case "toggle": toggle(x,arr); break;
+        case "all": all(arr); break;
+        case "empty": empty(arr); break;
+        default: break;
+    }
 }
 sr.Close();
 sw.Close();
 static void add(int i, int[] arr)
 {
-    if (arr[i-1] == 0) arr[i - 1] = 1;
+    if (arr[i - 1] == 0) arr[i - 1] = 1;
 }
 static void remove(int i, int[] arr)
 {
-    if (arr[i-1] == 1) arr[i - 1] = 0;
+    if (arr[i - 1] == 1) arr[i - 1] = 0;
 }
 static int check(int i, int[] arr)
 {
-    if (arr[i-1] == 1) return 1;
+    if (arr[i - 1] == 1) return 1;
     else return 0;
 }
 static void toggle(int i, int[] arr)
 {
-    if(arr[i-1] == 0) arr[i - 1] = 1;
-    else arr[i-1] = 0;
+    if (arr[i - 1] == 0) arr[i - 1] = 1;
+    else arr[i - 1] = 0;
 }
 static void all(int[] arr)
 {
