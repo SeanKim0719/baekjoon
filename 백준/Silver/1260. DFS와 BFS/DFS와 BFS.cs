cf33a445fc4c75bb29e1c173732f03b1856f903b
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-08-19 13:15:05 +0900

    [Silver II] Title: DFS와 BFS, Time: 124 ms, Memory: 8880 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cs
@@ -0,0 +1,65 @@
+var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int N = input[0]; int M = input[1]; int V = input[2];
+List<int>[] node = new List<int>[N+1];
+bool[] visit_DFS = new bool[N+1];
+bool[] visit_BFS = new bool[N+1];
+List<int> route_DFS = new List<int>();
+List<int> route_BFS = new List<int>();
+for (int i = 1; i <= N; i++)
+{
+    node[i] = new List<int>();
+}
+for (int i = 0; i < M; i++)
+{
+    var input_2 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    node[input_2[0]].Add(input_2[1]);
+    node[input_2[1]].Add(input_2[0]);
+}
+for(int i = 1; i <= N ; i++)
+{
+    node[i].Sort();
+}
+
+DFS(V, node, route_DFS, visit_DFS);
+BFS(V, node, route_BFS, visit_BFS);
+foreach(int i in route_DFS)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+foreach (int i in route_BFS)
+{
+    Console.Write(i + " ");
+}
+
+static void DFS(int node, List<int>[] edge, List<int> route, bool[] visit)
+{
+    visit[node] = true;
+    route.Add(node);
+    foreach(int i in edge[node])
+    {
+        if (!visit[i])
+        {
+            DFS(i, edge, route, visit);
+        }
+    }
+}
+
+static void BFS(int node, List<int>[] edge, List<int> route, bool[] visit)
+{
+    Queue<int> queue = new Queue<int>();
+    queue.Enqueue(node);
+    while(queue.Count > 0)
+    {
+        int x = queue.Dequeue();
+        visit[x] = true;
+        if(!route.Contains(x)) route.Add(x);
+        foreach (int i in edge[x])
+        {
+            if (!visit[i])
+            {
+                queue.Enqueue(i);
+            }
+        }
+    }
+}
