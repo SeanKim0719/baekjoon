a81ef0c68b3d35f75fc4a888db6be334a0f154e7
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-08-23 00:03:07 +0900

    [Silver V] Title: 나이순 정렬, Time: 116 ms, Memory: 19664 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cs
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cs
@@ -1,16 +1,19 @@
 StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
 StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
 int N = int.Parse(sr.ReadLine());
-var age = new List<(int, string)>();
-for(int i = 0; i < N; i++)
+var age = Enumerable.Range(0, 201).Select(x => new List<string> { x.ToString() }).ToList();
+for (int i = 0; i < N; i++)
 {
     var input = sr.ReadLine().Split();
-    age.Add((int.Parse(input[0]), input[1]));
+    age[int.Parse(input[0])].Add(input[1]); // age[21] = "21""JUNKUK"
 }
-var ans = age.OrderBy(x => x.Item1);
+var ans = age.Where(x => x.Count() > 1);
 foreach (var x in ans)
 {
-    sw.WriteLine($"{x.Item1} {x.Item2}");
+    for (int i = 1; i < x.Count; i++)
+    {
+        sw.WriteLine($"{x[0]} {x[i]}");
+    }
 }
 sr.Close();
 sw.Close();
