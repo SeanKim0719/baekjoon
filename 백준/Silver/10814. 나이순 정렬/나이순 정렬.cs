6b456e7802d5c805a624dd3f999ff61b08e17ea4
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-08-22 23:49:33 +0900

    [Silver V] Title: 나이순 정렬, Time: 2896 ms, Memory: 20324 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cs
@@ -0,0 +1,15 @@
+int N = int.Parse(Console.ReadLine());
+var age = Enumerable.Range(0, 201).Select(x => new List<string> {x.ToString()}).ToList();
+for(int i = 0; i < N; i++)
+{
+    var input = Console.ReadLine().Split();
+    age[int.Parse(input[0])].Add(input[1]); // age[21] = "21""JUNKUK"
+}
+var ans = age.Where(x => x.Count() > 1);
+foreach(var x in ans)
+{
+    for(int i = 1; i < x.Count; i++)
+    {
+        Console.WriteLine($"{x[0]} {x[i]}");
+    }
+}
