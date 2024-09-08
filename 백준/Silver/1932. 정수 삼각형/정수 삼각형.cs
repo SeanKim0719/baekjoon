9fabcef02d9b15485f548dd47cda09900f6294f6
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-09-08 19:24:24 +0900

    [Silver I] Title: 정수 삼각형, Time: 76 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cs
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cs
@@ -1,27 +1,21 @@
 var n = int.Parse(Console.ReadLine());
 var tri = new List<List<int>>();
-var DP = new int[n,n];
+var DP = new List<List<int>>();
 for (int i = 0; i < n; i++)
 {
     tri.Add(new List<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse)));
 }
-DP[0, 0] = tri[0][0];
 for(int i = 1; i < n; i++)
 {
     for(int j = 0; j < i+1; j++)
     {
-        if (j == 0) { DP[i, j] = DP[i - 1, j] + tri[i][j]; }
-        else if (j == i) { DP[i, j] = DP[i - 1, j - 1] + tri[i][j]; }
+        if (j == 0) { tri[i][j] = tri[i - 1][j] + tri[i][j]; }
+        else if (j == i) { tri[i][j] = tri[i - 1][j - 1] + tri[i][j]; }
         else
         {
-            DP[i, j] = tri[i][j] + Math.Max(DP[i - 1, j - 1], DP[i - 1, j]);
+            tri[i][j] = tri[i][j] + Math.Max(tri[i - 1][j - 1], tri[i - 1][j]);
         }
         
     }
 }
-int max = 0;
-for(int i = 0; i < n; i++)
-{
-    max = DP[n - 1, i] > max ? max = DP[n - 1, i] : max;
-}
-Console.WriteLine(max);
+Console.WriteLine(tri[^1].Max());
