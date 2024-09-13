0872054dc3975dd20aadbe7649253fc3d63dfdff
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-09-13 22:50:15 +0900

    [Gold IV] Title: 최단경로, Time: 340 ms, Memory: 27948 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cs
+++ 백준/Gold/1753. 최단경로/최단경로.cs
@@ -2,7 +2,6 @@ var input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
 var start = int.Parse(Console.ReadLine());
 var edge = Enumerable.Range(0, input[0]+1).Select(x => new List<(int node,int distance)>()).ToList();
 var dis = Enumerable.Repeat(-1, input[0]).ToArray();
-var visit = new bool[input[0] + 1];
 for(int i = 0; i < input[1]; i++)
 {
     var len = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
@@ -11,29 +10,25 @@ for(int i = 0; i < input[1]; i++)
 dijkstra(start);
 var ans = dis.Select(x => x == -1? "INF":$"{x}");
 Console.WriteLine(string.Join("\n",ans));
-
 void dijkstra(int start)
 {
-    var pq = new PriorityQueue<int,int>();
-    pq.Enqueue(start,0);
+    var pq = new PriorityQueue<(int pos,int distance),int>();
+    pq.Enqueue((start,0),0);
     dis[start-1] = 0;
     while (pq.Count > 0)
     {
         var node = pq.Dequeue();
-        if (!visit[node])
+        foreach (var next in edge[node.pos])
         {
-            visit[node] = true;
-            foreach (var next in edge[node])
+            if (dis[next.node - 1] == -1)
             {
-                if (dis[next.node-1] == -1)
-                {
-                    dis[next.node-1] = dis[node-1] + next.distance;
-                }
-                else
-                {
-                    dis[next.node-1] = Math.Min(dis[next.node-1], dis[node-1] + next.distance);
-                }
-                pq.Enqueue(next.node,dis[next.node-1]);
+                dis[next.node - 1] = next.distance + dis[node.pos-1];
+                pq.Enqueue(next, dis[next.node - 1]);
+            }
+            else if (dis[node.pos-1] + next.distance < dis[next.node-1])
+            {
+                dis[next.node-1] = dis[node.pos-1] + next.distance;
+                pq.Enqueue(next, dis[next.node - 1]);
             }
         }
     }
