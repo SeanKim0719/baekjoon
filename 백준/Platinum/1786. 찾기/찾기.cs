cb67211a4fd8ccf701d6a1621feefdc815f01645
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-09-23 21:51:21 +0900

    [Platinum V] Title: 찾기, Time: 492 ms, Memory: 46872 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.cs
@@ -0,0 +1,49 @@
+var str = Console.ReadLine();
+var pattern = Console.ReadLine();
+var ans = KMP(str, pattern, Get_Pat(pattern, pattern.Length));
+Console.WriteLine(ans.Item1);
+Console.WriteLine(string.Join(" ",ans.Item2));
+
+static int[] Get_Pat(string pattern,int size)
+{
+    var pat = new int[size];
+    int i = 1, j = 0;
+    while (i < pattern.Length)
+    {
+        if (pattern[i] == pattern[j])
+        {
+            pat[i] = j + 1;
+            i++; j++;
+        }
+        else if (j > 0) { j = pat[j - 1]; }
+        else { pat[i] = 0; i++; }
+    }
+    return pat;
+}
+
+static (int, List<int>) KMP(string text,string pattern,int[] pat)
+{
+    int n = text.Length, m = pattern.Length;
+    int i = 0, j = 0;
+    var cnt = (0,new List<int>());
+    while (i < n)
+    {
+        if (text[i] == pattern[j])
+        {
+            if (j == m - 1)
+            { 
+                cnt.Item1++;
+                cnt.Item2.Add(i-j+1);
+                j = pat[j]; 
+                i++; 
+            }
+            else { i++; j++; }
+        }
+        else
+        {
+            if (j > 0) { j = pat[j - 1]; }
+            else { i++; }
+        }
+    }
+    return cnt;
+}
