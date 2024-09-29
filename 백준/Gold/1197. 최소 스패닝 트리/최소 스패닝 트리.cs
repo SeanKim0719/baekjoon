280be1180d4491d3f0eb0e164a317f2f048de993
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-09-29 19:51:25 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 136 ms, Memory: 24464 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cs
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cs
@@ -3,7 +3,7 @@ int v = input[0], e = input[1];
 var edges = new PriorityQueue<(int start, int end, int weight), int>();
 var root = Enumerable.Range(0, v + 1).ToArray();
 var rank = Enumerable.Repeat(0, v + 1).ToArray();
-int ans = 0;
+int ans = 0, components = v;
 for(int i = 0; i < e; i++)
 {
     var edge = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
@@ -11,17 +11,17 @@ for(int i = 0; i < e; i++)
     edges.Enqueue((start,end,weight), weight);
 }
 
-while (edges.Count > 0)
+while (edges.Count > 0 && components > 1)
 {
     var edge = edges.Dequeue();
     if(Find(edge.start) != Find(edge.end))
     {
         Union(edge.start, edge.end);
         ans += edge.weight;
+        components--;
     }
 }
 Console.WriteLine(ans);
-
 int Find(int x)
 {
     if (root[x] == x) { return x; }
@@ -30,16 +30,12 @@ int Find(int x)
         return root[x] = Find(root[x]);
     }
 }
-
 void Union(int x, int y)
 {
     x = Find(x);
     y = Find(y);
     if(x == y) { return; }
-    if (rank[x] == rank[y]) { root[x] = y; }
-    else
-    {
-        root[y] = x;
-        if (rank[x] == rank[y]) { rank[x]++; }
-    }
+    if (rank[x] > rank[y]) { root[y] = x; }
+    else if (rank[x] < rank[y]) { root[x] = y; }
+    else { root[y] = x; rank[x]++; }
 }
