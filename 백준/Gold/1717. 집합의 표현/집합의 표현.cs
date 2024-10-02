ab4f0a40afb4b8d890cc13914e1478a417b02b9a
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-10-02 21:11:42 +0900

    [Gold V] Title: 집합의 표현, Time: 2932 ms, Memory: 27588 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cs
@@ -0,0 +1,51 @@
+using System.Xml;
+
+var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0], m = input[1];
+var set = Enumerable.Range(0, n + 1).ToArray();
+var rank = Enumerable.Repeat(0, n + 1).ToArray();
+for (int i = 0; i < m; i++)
+{
+    var ope = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    if (ope[0] == 0)
+    {
+        if (Find(ope[1]) != Find(ope[2])) { Union(ope[1], ope[2]); }
+    }
+    else
+    {
+        Console.WriteLine(Find(ope[1]) == Find(ope[2]) ? "YES" : "NO");
+    }
+}
+int Find(int x)
+{
+    if (set[x] == x)
+    {
+        return x;
+    }
+    else
+    {
+        return set[x] = Find(set[x]);
+    }
+}
+
+void Union(int x, int y)
+{
+    x = Find(x);
+    y = Find(y);
+    if (x == y)
+    {
+        return;
+    }
+    if (rank[x] > rank[y])
+    {
+        set[y] = x; rank[y] = rank[x];
+    }
+    else if (rank[x] < rank[y])
+    {
+        set[x] = y; rank[y] = rank[x];
+    }
+    else
+    {
+        set[y] = x; rank[x]++; rank[y] = rank[x];
+    }
+}
