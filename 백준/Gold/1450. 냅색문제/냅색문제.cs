6d1826d25b63907ae3a141bcd2564275ea9afab9
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-10-12 19:56:50 +0900

    [Gold I] Title: 냅색문제, Time: 140 ms, Memory: 8428 KB -BaekjoonHub

---
+++ 백준/Gold/1450. 냅색문제/냅색문제.cs
@@ -0,0 +1,37 @@
+var input = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+long n = input[0], c = input[1];
+var items = Console.ReadLine().Split().Select(long.Parse).ToArray();
+int mid = items.Length / 2;
+var items_left = items[0..mid];
+var items_right = items[mid..items.Length];
+var sums_left = new List<long>() { 0 };
+var sums_right = new List<long>() { 0 };
+long ans = 0;
+foreach (var num in items_left)
+{
+    var new_sums = new List<long>();
+    foreach (var sum in sums_left)
+    {
+        new_sums.Add(num + sum);
+    }
+    sums_left.AddRange(new_sums);
+}
+foreach (var num in items_right)
+{
+    var new_sums = new List<long>();
+    foreach (var sum in sums_right)
+    {
+        new_sums.Add(num + sum);
+    }
+    sums_right.AddRange(new_sums);
+}
+sums_right.Sort();
+foreach (var sum in sums_left)
+{
+    long remaining = c - sum;
+    var point = sums_right.BinarySearch(remaining);
+    if (point < 0) { point = ~point; }
+    else { while (point < sums_right.Count && sums_right[point] == remaining) { point++; } }
+    ans += point;
+}
+Console.WriteLine(ans);
