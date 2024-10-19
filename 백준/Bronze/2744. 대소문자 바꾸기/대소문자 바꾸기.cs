0e45ab9f33952ac966866f23db557af141cebc0c
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-10-19 12:24:24 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 48 ms, Memory: 5364 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cs
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cs
@@ -1,2 +1,1 @@
-var input = Console.ReadLine().Select(x=> char.IsLower(x)? char.ToUpper(x):char.ToLower(x));
-Console.WriteLine(string.Join("",input));
+foreach(var a in Console.ReadLine())Console.Write((char)(a>96?a-32:a+32));
