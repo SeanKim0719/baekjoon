eb1373e785300ebc8fa62a3b757b6647c93a977b
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-10-21 13:18:37 +0900

    [Bronze III] Title: 학점계산, Time: 52 ms, Memory: 6248 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cs
@@ -0,0 +1,12 @@
+var grade = new List<(string, double)>()
+{
+    ("A+",4.3), ("A0",4.0), ("A-",3.7),("B+",3.3),("B0",3.0),("B-",2.7),
+    ("C+",2.3),("C0",2.0),("C-",1.7),("D+",1.3),("D0",1.0),("D-",0.7),("F",0.0) };
+var input = Console.ReadLine();
+foreach(var g in grade)
+{
+    if(input == g.Item1)
+    {
+        Console.WriteLine($"{g.Item2:N1}");
+    }
+}
