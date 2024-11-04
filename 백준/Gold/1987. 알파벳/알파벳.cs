c5fb13deba911e1c19b41fc726b2796f6319e909
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-11-04 20:32:50 +0900

    [Gold IV] Title: 알파벳, Time: 656 ms, Memory: 5864 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cs
+++ 백준/Gold/1987. 알파벳/알파벳.cs
@@ -2,15 +2,15 @@ var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int r = input[0], c = input[1];
 var board = new List<string>();
 var visit = new bool[26];
-var ans = new List<int>();
 int cnt = 1;
+int ans = 0;
 for(int i = 0; i < r; i++) { board.Add(Console.ReadLine()); }
 dfs(0, 0, 1);
-Console.WriteLine(ans.Max());
+Console.WriteLine(ans);
 void dfs(int x, int y, int cnt)
 {
     visit[board[x][y] - 'A'] = true;
-    ans.Add(cnt); 
+    ans = Math.Max(ans, cnt);
     if (x + 1 < r && !visit[board[x + 1][y] - 'A'])
     {
         visit[board[x + 1][y] - 'A'] = true;
