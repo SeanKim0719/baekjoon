497abe2d1014e7fb145c5b90e231c4b8d1d769ef
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-11-05 23:08:34 +0900

    [Gold IV] Title: 문자열 폭발, Time: 220 ms, Memory: 23900 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cs
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cs
@@ -1,21 +1,14 @@
+using System.Text;
 string str = Console.ReadLine();
 string burst = Console.ReadLine();
-var check = new Stack<char>();
+var sb = new StringBuilder();
 foreach (char c in str)
 {
-    check.Push(c);
-    if(check.Count >= burst.Length)
+    sb.Append(c);
+    if(sb.Length >= burst.Length)
     {
-        var temp = new List<char>();
-        for (int i = 0; i < burst.Length; i++) { temp.Add(check.Pop()); }
-        temp.Reverse();
-        if (new string(temp.ToArray()) != burst)
-        {
-            foreach (var t in temp)
-            {
-                check.Push(t);
-            }
-        }
+        var temp = sb.ToString(sb.Length-burst.Length,burst.Length);
+        if(temp == burst) { sb.Remove(sb.Length - burst.Length, burst.Length); }
     }   
 }
-Console.WriteLine(check.Count == 0? "FRULA":new string(check.Reverse().ToArray()));
+Console.WriteLine(sb.Length == 0? "FRULA":sb.ToString());
