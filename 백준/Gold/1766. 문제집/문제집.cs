d518b99dd02ce5665797b9e5640ef3a6d2e23b47
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-11-08 18:59:39 +0900

    [Gold II] Title: 문제집, Time: 168 ms, Memory: 22784 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.cs
+++ 백준/Gold/1766. 문제집/문제집.cs
@@ -1,31 +1,24 @@
-var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+var input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
 int n = input[0],m = input[1];
 var pre = Enumerable.Range(0,n+1).Select(x=>new List<int>()).ToList();
 var rank = Enumerable.Repeat(0,n+1).ToArray();
 var pq = new PriorityQueue<int, int>();
 var ans = new List<int>();
-for(int i = 0; i < m; i++)
+while(m-- > 0)
 {
-    var info = Console.ReadLine().Split().Select(int.Parse).ToList();
+    var info = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
     pre[info[0]].Add(info[1]);
     rank[info[1]]++;
 }
-for(int i = 1; i <= n; i++)
-{
-    if (rank[i] == 0) { pq.Enqueue(i, i); }
-    
-}
+for (int i = 1; i <= n; i++) { if (rank[i] == 0) { pq.Enqueue(i, i); } }
 while (pq.Count > 0)
 {
     var solve = pq.Dequeue();
     ans.Add(solve);
-    if (pre[solve].Count > 0)
+    foreach (var i in pre[solve])
     {
-        foreach(var i in pre[solve])
-        {
-            rank[i]--;
-            if (rank[i] == 0) { pq.Enqueue(i, i); }
-        }
+        rank[i]--;
+        if (rank[i] == 0) { pq.Enqueue(i, i); }
     }
 }
 Console.WriteLine(string.Join(" ",ans));
