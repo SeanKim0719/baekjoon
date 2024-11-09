92357b92c7f86f779d1e9cfcaa16ae6303dffe83
SeanKim0719 <76403430+SeanKim0719@users.noreply.github.com>
2024-11-09 22:04:08 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 816 ms, Memory: 108536 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cs
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cs
@@ -4,15 +4,8 @@ var n_list = read().Split().Select(int.Parse).ToList();
 var m = int.Parse(read());
 var m_list = read().Split().Select(int.Parse).ToList();
 var dic = new Dictionary<int,int>();
-var ans = new List<int>();
 foreach (var x in n_list)
 {
-    if (dic.ContainsKey(x)) dic[x]++;
-    else dic.Add(x,1);
+    if (!dic.TryAdd(x,1)) dic[x]++;
 }
-foreach(var y in m_list)
-{
-    if (dic.ContainsKey(y)) ans.Add(dic[y]);
-    else ans.Add(0);
-}
-Console.WriteLine(string.Join(" ",ans));
+Console.WriteLine(string.Join(" ",m_list.Select(x => dic.ContainsKey(x) ? dic[x]:0)));
